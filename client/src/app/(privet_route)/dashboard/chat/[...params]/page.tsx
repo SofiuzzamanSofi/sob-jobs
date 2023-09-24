df4f7467a987b16a496e095af17b58322ad828b4
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-09-24 15:39:38 +0600

    message inbox is ready to chokhom

--- client/src/app/(privet_route)/dashboard/chat/[...params]/page.tsx
+++ client/src/app/(privet_route)/dashboard/chat/[...params]/page.tsx
@@ -7,6 +7,10 @@ import Loading from '@/components/Loading';
 import { useRouter } from 'next/navigation';
 import React, { FC, useRef, useState, useEffect } from 'react';
 import { useGetMessageByIdQuery, usePostMessageByIdMutation } from '@/redux/features/message/messageApi';
+import uploadImageIcon from "@/assets/upload-image.svg";
+import chatImojiIcon from "@/assets/chat-imoji.svg";
+import { BsArrowRightShort } from "react-icons/bs";
+import Image from "next/image";
 
 interface PageProps {
     params: {
@@ -65,6 +69,14 @@ const Page: FC<PageProps> = ({ params, }) => {
     // other user not seen other chat 
     const matchUser = messageDetailsData?.data.participants.filter((userInfo) => userInfo.userEmail === reduxStore.auth.user?.email && userInfo.userId === reduxStore.auth.user?._id)
 
+    // className 
+    const imogiButtonClass = "p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-200 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+
+    const replyAnsButtonClass = "shrink-0 h-10 w-10 bg-primary/10 border border-primary dark:border-darkPrimary hover:bg-primary rounded-full grid place-items-center text-primary dark:text-darkPrimary hover:text-white hover:px-2 transition-all"
+
+    const textAreaClass = "lg:mx-4 p-2 w-full border border-gray-200 dark:border-gray-700 rounded-md bg-gray-100 dark:bg-gray-900 text-slate-700 dark:text-slate-400"
+
+
     if (messageDetailsIsLoading) {
         return <Loading />
     };
@@ -86,7 +98,7 @@ const Page: FC<PageProps> = ({ params, }) => {
         const participantsOthers = messageDetailsData.data.participants.filter((p) => p.userEmail !== reduxStore.auth.user?.email);
 
         return (
-            <div>
+            <div className='py-5'>
                 <div>
                     <h1>{participantsOthers[0]?.userName}</h1>
                     <h1>{participantsOthers[0]?.userEmail}</h1>
@@ -106,7 +118,7 @@ const Page: FC<PageProps> = ({ params, }) => {
                         ))
                     }
                 </div>
-                <div>
+                {/* <div>
                     <textarea
                         className='border p-2'
                         placeholder='Write a message'
@@ -116,35 +128,36 @@ const Page: FC<PageProps> = ({ params, }) => {
                         onKeyDown={(e) => functionCallOnPressInter(e)}
                     />
                     <button onClick={submitMessage}>Submit</button>
-                </div>
+                </div> */}
                 <div>
 
-                    <form>
+                    <div>
                         <label htmlFor="chat" className="sr-only">Your message</label>
                         <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
-                            <button type="button" className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                    <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
-                                </svg>
+                            <button type="button" className={imogiButtonClass}>
+                                <Image className="" src={uploadImageIcon} alt='upload-image-icon' />
                                 <span className="sr-only">Upload image</span>
                             </button>
-                            <button type="button" className="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z" />
-                                </svg>
+                            <button type="button" className={imogiButtonClass}>
+                                <Image className="" src={chatImojiIcon} alt='chat-imoji-icon' />
                                 <span className="sr-only">Add emoji</span>
                             </button>
-                            <textarea id="chat" rows={1} className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
-                            <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
-                                <svg className="w-5 h-5 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                                    <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-                                </svg>
-                                <span className="sr-only">Send message</span>
+                            <textarea
+                                id="chat" rows={1} className={textAreaClass} placeholder="Your message..."
+                                ref={textAreaRef}
+                                value={text}
+                                onChange={(e) => setText(e.target.value)}
+                                onKeyDown={(e) => functionCallOnPressInter(e)}
+                            ></textarea>
+                            <button
+                                className={replyAnsButtonClass}
+                                type="submit"
+                                onClick={submitMessage}
+                            >
+                                <BsArrowRightShort size={30} />
                             </button>
                         </div>
-                    </form>
+                    </div>
 
                 </div>
             </div>
